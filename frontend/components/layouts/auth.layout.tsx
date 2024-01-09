291cc277df6323e7cab7060b2815b14044005900
Christian Daher <christian@tomorrow.services>
2024-01-10 01:04:53 +0200

    working navbar with main content

--- frontend/components/layouts/auth.layout.tsx
+++ frontend/components/layouts/auth.layout.tsx
@@ -1,16 +1,24 @@
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
+import { useContext } from "react";
+import { NavbarContext } from "@/components/contexts/navbar.context";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isOpen } = useContext(NavbarContext);
+
   return (
     <>
       <Header />
       <Navbar />
-      <main className="pt-24 px-8 pb-8">{children}</main>
+      <main
+        className={`pt-24 px-8 pb-8 transform transition-all duration-200 pointer-events-none ${isOpen ? "w-[calc(100%-17rem)] translate-x-[17rem]" : ""}`}
+      >
+        {children}
+      </main>
     </>
   );
 }
