b6a6cde8bb345c67d59fb9213f53d407ba8a433a
ChristianDaher <cmd10011@students.aust.edu.lb>
2024-02-13 18:00:04 +0200

    SO SLOW

--- backend/database/sequelize.ts
+++ backend/database/sequelize.ts
@@ -6,8 +6,9 @@ require("dotenv").config();
 export default new Sequelize({
   dialect: "mysql",
   database: process.env.DB_NAME || "employees",
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
   host: process.env.DB_HOST || "127.0.0.1",
   port: Number(process.env.DB_PORT) || 3306,
   models: [path.join(__dirname, "models")],
